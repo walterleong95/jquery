62722fb1dedb0003a3622cd5c64acfe94a28009f
walterleong95 <leonghoiweng@hotmail.com>
2017-08-29 02:12:23 +0800

    try

--- push.sh
+++ push.sh
@@ -6,6 +6,7 @@ setup_git(){
 }
 
 commit_report_files(){
+	git checkout scc_project
 	git add .
 	git commit -m "$TRAVIS_BUILD_NUMBER [skip ci]"
 }
@@ -13,8 +14,8 @@ commit_report_files(){
 upload_files(){
 	echo GH_TOKEN=c48bcc6374c50c68df89f84045b4319b70630001
 
-	git remote add origin scc_project https://walterleong95:$GITHUB_API_KEY@github.com/walterleong95/jquery.git
-	git push --set-upstream origin scc_project
+	git remote add origin-scc_project https://walterleong95:$GITHUB_API_KEY@github.com/walterleong95/jquery.git
+	git push --set-upstream origin-scc_project scc_project
 	git branch
 }
 
