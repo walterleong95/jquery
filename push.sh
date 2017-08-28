472499ce4c6bd631daaa017ebb5a6566f706b46b
walterleong95 <leonghoiweng@hotmail.com>
2017-08-29 00:19:54 +0800

    updated api key token

--- push.sh
+++ push.sh
@@ -12,10 +12,10 @@ commit_report_files(){
 }
 
 upload_files(){
-	echo GH_TOKEN=7ab01452590ca573641dd950246c30f4d2c7d4f8
+	echo GH_TOKEN=c48bcc6374c50c68df89f84045b4319b70630001
 
 	git remote add origin-scc_project https://walterleong95:$GITHUB_API_KEY@github.com/walterleong95/jquery.git
-	git push --quiet --set-upstream origin-scc_project scc_project
+	git push --set-upstream origin-scc_project scc_project
 }
 
 	  setup_git
